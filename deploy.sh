034ca687d617a30ea33dfbdcad403e161cc5df98
Lazuardi N Putra <lazuardi.putra@tokopedia.com>
2020-11-01 23:56:31 +0700

    move to nvim

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-# VIM Automation
-mkdir -p $HOME/.vim/snippets
-ln -s $(pwd)/vim/vimrc $HOME/.vimrc
-ln -s $(pwd)/vim/py_header $HOME/.vim/py_header
-ln -s $(pwd)/vim/python.snippets $HOME/.vim/snippets/python.snippets
+NVIM_DIR="$HOME/.config/nvim"
+
+# NVIM Automation
+sudo pip3 install pynvim
+if [ ! -d "$NVIM_DIR" ]; then mkdir -p "$NVIM_DIR"; fi
+if [ ! -d "$NVIM_DIR/snippets" ]; then mkdir -p "$NVIM_DIR/snippets"; fi
+if [ ! -f "$NVIM_DIR/init.vim" ]; then ln -s $(pwd)/vim/vimrc $NVIM_DIR/init.vim; fi
+if [ ! -f "$NVIM_DIR/py_header" ]; then ln -s $(pwd)/vim/py_header $NVIM_DIR/py_header; fi
+if [ ! -f "$NVIM_DIR/snippets/python.snippets" ]; then ln -s $(pwd)/vim/python.snippets $NVIM_DIR/snippets/python.snippets; fi
