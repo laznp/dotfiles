e5d6dc7cef2112b9104cb7d69381a37720a86040
Lazuardi N Putra <me@laznp.id>
2020-12-23 00:13:34 +0700

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -12,3 +12,9 @@ if [ ! -f "$NVIM_DIR/snippets/python.snippets" ]; then ln -s $(pwd)/vim/python.s
 
 # ZSH Aliases
 if [ ! -f "$HOME/.zsh_alias" ]; then ln -s $(pwd)/zsh/zsh_alias $HOME/.zsh_alias; fi
+
+# Automate i3, i3blocks and Alacritty
+if [ ! -d "$HOME/.config" ]; then mkdir -p "$HOME/.config"; fi
+ln -s $(pwd)/i3 "$HOME/.config/i3"
+ln -s $(pwd)/i3blocks "$HOME/.config/i3blocks"
+ln -s $(pwd)/alacritty "$HOME/.config/alacritty"
