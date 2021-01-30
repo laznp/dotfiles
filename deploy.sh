21c5432777712ca39f7c9ce3081a5421d530b355
Lazuardi N Putra <me@laznp.id>
2021-01-31 00:45:23 +0700

    add battery state

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 NVIM_DIR="$HOME/.config/nvim"
 
 # NVIM Automation
-# sudo pip3 install pynvim # NeoVim Python3 Support
+sudo pip3 install pynvim # NeoVim Python3 Support
 if [ ! -d "$NVIM_DIR" ]; then mkdir -p "$NVIM_DIR"; fi
 if [ ! -d "$NVIM_DIR/snippets" ]; then mkdir -p "$NVIM_DIR/snippets"; fi
 if [ ! -f "$NVIM_DIR/init.vim" ]; then ln -s $(pwd)/vim/vimrc $NVIM_DIR/init.vim; fi
@@ -12,7 +12,9 @@ if [ ! -f "$NVIM_DIR/snippets/python.snippets" ]; then ln -s $(pwd)/vim/python.s
 
 # ZSH
 if [ ! -f "$HOME/.zsh_alias" ]; then ln -s $(pwd)/zsh/zsh_alias $HOME/.zsh_alias; fi
-echo "source ~/.zsh_alias" >> ~/.zshrc
+
+# ZSH Alias
+if [ "$(grep "zsh_alias" ~/.zshrc | wc -l)" == "0" ]; then echo "source ~/.zsh_alias" >> ~/.zshrc; fi
 
 # Automate i3, i3blocks and Alacritty
 if [ ! -d "$HOME/.config" ]; then mkdir -p "$HOME/.config"; fi
---
+++ i3blocks/battery-state.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+GET_PERCENTAGE=$(acpi -b | awk -F ',' '{print $2}' | tr -d ' ')
+GET_STATE=$(acpi -b | awk -F ',' '{print $1}' | awk -F ':' '{ print $2 }' | tr -d ' ')
+
+if [ "$GET_STATE" == "Charging" ];  then
+	echo "$GET_STATE $GET_PERCENTAGE"
+elif [ "$GET_STATE" == "Full" ]; then
+	echo "Full $GET_PERCENTAGE"
+else
+	echo "$GET_PERCENTAGE"
+fi
