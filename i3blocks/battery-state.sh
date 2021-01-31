6a94b4af54af5c09012085ed4699996559c38caf
Lazuardi N Putra <me@laznp.id>
2021-01-31 18:23:38 +0700

    add new revision

--- i3blocks/battery-state.sh
+++ i3blocks/battery-state.sh
@@ -2,11 +2,10 @@
 
 GET_PERCENTAGE=$(acpi -b | awk -F ',' '{print $2}' | tr -d ' ')
 GET_STATE=$(acpi -b | awk -F ',' '{print $1}' | awk -F ':' '{ print $2 }' | tr -d ' ')
-
 if [ "$GET_STATE" == "Charging" ];  then
-	echo "$GET_STATE $GET_PERCENTAGE"
+	echo "$GET_PERCENTAGE ﮣ"
 elif [ "$GET_STATE" == "Full" ]; then
 	echo "Full $GET_PERCENTAGE"
 else
-	echo "$GET_PERCENTAGE"
+	echo " $GET_PERCENTAGE"
 fi
