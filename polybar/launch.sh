27c5ba5f5723320527da6eee2a5e1a4f14e452b9
Lazuardi N Putra <me@laznp.id>
2021-02-06 01:08:37 +0700

    add for polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+# Launch
+polybar --list-monitors | awk -F ':' '{ print $1 }' | tr -d ' ' | while read line;
+do
+	polybar $line &
+done
+
+echo "Bar launched..."
+
