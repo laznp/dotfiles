3e7ca185898fe24b30a8ab21b5916420a3ee1125
Lazuardi N Putra <me@laznp.id>
2021-02-09 02:03:29 +0700

    .

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,8 @@ NVIM_DIR="$HOME/.config/nvim"
 # NVIM Automation
 sudo pip3 install pynvim # NeoVim Python3 Support
 if [ ! -d "$NVIM_DIR" ]; then mkdir -p "$NVIM_DIR"; fi
-if [ ! -d "$NVIM_DIR/snippets" ]; then mkdir -p "$NVIM_DIR/snippets"; fi
 if [ ! -f "$NVIM_DIR/init.vim" ]; then ln -s $(pwd)/vim/vimrc $NVIM_DIR/init.vim; fi
-if [ ! -f "$NVIM_DIR/py_header" ]; then ln -s $(pwd)/vim/py_header $NVIM_DIR/py_header; fi
-if [ ! -f "$NVIM_DIR/go_header" ]; then ln -s $(pwd)/vim/go_header $NVIM_DIR/go_header; fi
+if [ ! -d "$NVIM_DIR/template" ]; then ln -s $(pwd)/vim/template $NVIM_DIR/; fi
 
 # ZSH
 if [ ! -f "$HOME/.zsh_alias" ]; then ln -s $(pwd)/zsh/zsh_alias $HOME/.zsh_alias; fi
