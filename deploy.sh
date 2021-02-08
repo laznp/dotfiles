96a4fe038a275c9c7b12afc9e425376d815ad4e8
Lazuardi N Putra <me@laznp.id>
2021-02-09 01:48:48 +0700

    add autocomplete template for vim

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ if [ ! -d "$NVIM_DIR" ]; then mkdir -p "$NVIM_DIR"; fi
 if [ ! -d "$NVIM_DIR/snippets" ]; then mkdir -p "$NVIM_DIR/snippets"; fi
 if [ ! -f "$NVIM_DIR/init.vim" ]; then ln -s $(pwd)/vim/vimrc $NVIM_DIR/init.vim; fi
 if [ ! -f "$NVIM_DIR/py_header" ]; then ln -s $(pwd)/vim/py_header $NVIM_DIR/py_header; fi
-if [ ! -f "$NVIM_DIR/snippets/python.snippets" ]; then ln -s $(pwd)/vim/python.snippets $NVIM_DIR/snippets/python.snippets; fi
+if [ ! -f "$NVIM_DIR/go_header" ]; then ln -s $(pwd)/vim/go_header $NVIM_DIR/go_header; fi
 
 # ZSH
 if [ ! -f "$HOME/.zsh_alias" ]; then ln -s $(pwd)/zsh/zsh_alias $HOME/.zsh_alias; fi
