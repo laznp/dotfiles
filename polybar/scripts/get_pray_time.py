fac89c2e23744fa88aca4061bbfa56dc9263aafb
Lazuardi N Putra <me@laznp.id>
2021-10-20 22:05:09 +0700

    fix prayer time script

--- polybar/scripts/get_pray_time.py
+++ polybar/scripts/get_pray_time.py
@@ -3,21 +3,18 @@ import requests as curl
 import notify2
 from datetime import datetime, timedelta
 
-
 key_list = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha']
-today_date = datetime.strftime(datetime.now(), "%Y-%m-%d")
-URL = f"https://api.pray.zone/v2/times/day.json?city=bekasi&date={today_date}"
+URL = f"https://api.pray.zone/v2/times/today.json?city=jakarta&school=9"
 data = curl.get(URL).json()
 pray_datetime = data['results']['datetime']
 pray_time = [ x['times'] for x in pray_datetime ][0]
 time_now = datetime.strptime(datetime.strftime(datetime.now(), "%H:%M"), "%H:%M")
+
 for key in pray_time:
     if key in key_list:
-        # print(f"{key} -> {pray_time[key]}")
-        pray_time_now = datetime.strptime(pray_time[key], "%H:%M")
-        difference = datetime.strptime(str(pray_time_now - time_now), "%H:%M:%S")
-        if difference.hour < 2:
+        pray_time_next = datetime.strptime(pray_time[key], "%H:%M")
+        if time_now < pray_time_next:
+            difference = datetime.strptime(str(pray_time_next - time_now), "%H:%M:%S")
             print(f"{key} in {difference.hour}:{difference.minute}")
-        if pray_time_now == time_now:
-            notif = notify2.Notification("Islamic Prayer Notification", f"Time to shalah {key}")
+            break
 
