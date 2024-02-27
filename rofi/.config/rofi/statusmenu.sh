578cf2305fd34f5b16e60d2bab9c2dcb58fa9fad
Lazuardi N Putra <me@laznp.id>
2024-02-28 00:57:13 +0700

    update

--- rofi/.config/rofi/statusmenu.sh
+++ rofi/.config/rofi/statusmenu.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-dir="$HOME/.config/rofi"
-
-uptime=$(uptime -p | sed -e 's/up //g')
-
 rofi_command="rofi -theme $HOME/.config/rofi/card_square"
 
 # Options
@@ -16,13 +12,13 @@ gputemp="󰢮  GPU Temperature"
 # Variable passed to rofi
 options="$mousebat\n$headphonebat\n$datetime\n$cputemp\n$gputemp"
 
-chosen="$(echo -e "$options" | $rofi_command -lines 5 -p"﫵 $uptime" -dmenu -selected-row 2)"
+chosen="$(echo -e "$options" | $rofi_command -lines 5 -dmenu -selected-row 2)"
 case $chosen in
     $mousebat)
-        notify-send "Razer Basilisk Ultimate" $($HOME/.local/bin/razer_battery) -t 1000
+        notify-send "Razer Basilisk Ultimate" $($HOME/.local/bin/razer_battery) -t 1500
         ;;
     $headphonebat)
-        notify-send "Sony WH-1000XM4" "$(bluetoothctl info | grep "WH-1000XM4" -A30 | grep Battery | grep -oP '(?<=\().*(?=\))')%" -t 1000
+        notify-send "Sony WH-1000XM4" "$(bluetoothctl info | grep "WH-1000XM4" -A30 | grep Battery | grep -oP '(?<=\().*(?=\))')%" -t 1500
         ;;
     $datetime)
         notify-send "Date Time" "$(date "+%d %b %Y  %H:%M")" -t 1500
