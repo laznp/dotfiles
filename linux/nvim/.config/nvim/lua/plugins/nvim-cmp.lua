0e0414c1ae0ba9a7506f6cf599cd4023833ba6b2
Lazuardi N Putra <me@laznp.id>
2024-09-30 14:40:24 +0700

    update

--- linux/nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ linux/nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -12,7 +12,6 @@ return {
         "onsails/lspkind.nvim"
     },
     config = function()
-        -- Setup nvim-cmp.
         local cmp = require'cmp'
         local lspkind = require('lspkind')
         require("luasnip.loaders.from_vscode").lazy_load()
@@ -27,7 +26,7 @@ return {
                 ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
                 ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
                 ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
-                ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+                ['<C-y>'] = cmp.config.disable, 
                 ['<C-e>'] = cmp.mapping({
                         i = cmp.mapping.abort(),
                         c = cmp.mapping.close(),
@@ -53,7 +52,7 @@ return {
             formatting = {
                 fields = { "kind", "abbr", "menu" },
                 format = function(entry, vim_item)
-                  local kind = require("lspkind").cmp_format({ 
+                    local kind = require("lspkind").cmp_format({ 
                         mode = "symbol_text", 
                         maxwidth = 50,
                         menu = ({
@@ -64,10 +63,10 @@ return {
                             latex_symbols = "[Latex]",
                         })
                     })(entry, vim_item)
-                  local strings = vim.split(kind.kind, "%s", { trimempty = true })
-                  kind.kind = " " .. (strings[1] or "") .. " "
-                  kind.menu = "    (" .. (strings[2] or "") .. ")"
-                  return kind
+                    local strings = vim.split(kind.kind, "%s", { trimempty = true })
+                    kind.kind = " " .. (strings[1] or "") .. " "
+                    kind.menu = "    (" .. (strings[2] or "") .. ")"
+                    return kind
                 end,
             },
             sources = cmp.config.sources({
