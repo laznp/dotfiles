3b9b9ea375beb62e1970f574f87b53b735452dbe
Lazuardi N Putra <me@laznp.id>
2026-04-10 01:06:33 +0700

    main update

--- linux/nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ linux/nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,36 +1,36 @@
 return {
-    'MeanderingProgrammer/render-markdown.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-    opts = {
-        code = {
-            sign = false,
-            left_pad = 2,
-        },
-        heading = {
-            border = false,
-            sign = false,
-            position = 'inline',
-            icons = { ' ' }
-        },
-        indent = {
-            enabled = false,
-            skip_level = 0,
-            skip_heading = false,
-            render_modes = false,
-        },
-        checkbox = {
-            unchecked = { icon = '✘ ' },
-            checked = { icon = '✔ ' },
-            custom = { todo = { rendered = '◯ ' } },
-        },
-        anti_conceal = {
-            enabled = false,
-        },
-        pipe_table = { 
-            preset = 'heavy', 
-            filler = '',
-            head = '',
-            row = '',
-        },
-    },
+    -- 'MeanderingProgrammer/render-markdown.nvim',
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    -- opts = {
+        -- code = {
+            -- sign = false,
+            -- left_pad = 2,
+        -- },
+        -- heading = {
+            -- border = false,
+            -- sign = false,
+            -- position = 'inline',
+            -- icons = { ' ' }
+        -- },
+        -- indent = {
+            -- enabled = false,
+            -- skip_level = 0,
+            -- skip_heading = false,
+            -- render_modes = false,
+        -- },
+        -- checkbox = {
+            -- unchecked = { icon = '✘ ' },
+            -- checked = { icon = '✔ ' },
+            -- custom = { todo = { rendered = '◯ ' } },
+        -- },
+        -- anti_conceal = {
+            -- enabled = false,
+        -- },
+        -- pipe_table = {
+            -- preset = 'heavy',
+            -- filler = '',
+            -- head = '',
+            -- row = '',
+        -- },
+    -- },
 }
